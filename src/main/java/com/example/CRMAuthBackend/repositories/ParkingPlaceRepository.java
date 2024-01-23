ef1a1ac91962f593724470327e7a861add946aec
Sla1merAPK <slameryt4@gmail.com>
2024-01-23 17:53:37 +0200

    h

--- src/main/java/com/example/CRMAuthBackend/repositories/ParkingPlaceRepository.java
+++ src/main/java/com/example/CRMAuthBackend/repositories/ParkingPlaceRepository.java
@@ -11,8 +11,8 @@ public interface ParkingPlaceRepository extends JpaRepository<ParkingPlace, Inte
 
     @Query(value = "SELECT p FROM parking_places p " +
             "WHERE p.is_free = true AND p.park_id = :parkId" +
-            "ORDER BY SQRT((p.coordinate_x) * (p.coordinate_x) + " +
-            "(p.coordinate_y) * (p.coordinate_y)) " +
+            "ORDER BY SQRT((p.coordinatex) * (p.coordinatex) + " +
+            "(p.coordinatey) * (p.coordinatey)) " +
             "LIMIT 1", nativeQuery = true)
     ParkingPlace findNearestFreePlace(@Param("parkId") int parkId);
 }
