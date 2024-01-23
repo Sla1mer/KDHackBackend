69122b81cde279a52e659c06374caa44ca4d0f69
Sla1merAPK <slameryt4@gmail.com>
2024-01-23 17:49:49 +0200

    h

--- src/main/java/com/example/CRMAuthBackend/repositories/ParkingPlaceRepository.java
+++ src/main/java/com/example/CRMAuthBackend/repositories/ParkingPlaceRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.repository.query.Param;
 
 public interface ParkingPlaceRepository extends JpaRepository<ParkingPlace, Integer> {
 
-    ParkingPlace findByParkingPlaceNameAndParkId(String ParkingPlaceName, int parkId);
+    ParkingPlace findByParkPlaceNameAndParkId(String parkPlaceName, int parkId);
 
-    @Query(value = "SELECT * FROM parking_places p " +
+    @Query(value = "SELECT p FROM parking_places p " +
             "WHERE p.is_free = true AND p.park_id = :parkId" +
             "ORDER BY SQRT((p.coordinate_x) * (p.coordinate_x) + " +
             "(p.coordinate_y) * (p.coordinate_y)) " +
